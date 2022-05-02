3564a19e7ff6f2f52042905d1296ff0f286659e3
“VisionifyCS” <mayur@visionify.ai>
2022-05-02 15:13:56 +0530

    working on ui

--- src/Component/Upload.js
+++ src/Component/Upload.js
@@ -1,33 +1,118 @@
 import { Button } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import "./Upload.css";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: "auto",
+  height: 180,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
 
 const Upload = () => {
-  const [selectedFiles, setSelectedFiles] = useState([]);
-
-  const handleImageChange = (e) => {
-    // console.log(e.target.files)
-    if (e.target.files) {
-      const filesArray = Array.from(e.target.files).map((file) =>
-        URL.createObjectURL(file)
+  const [files, setFiles] = useState([]);
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+    open,
+  } = useDropzone({
+    accept: "image/*",
+    noClick: true,
+    noKeyboard: true,
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
       );
+    },
+  });
 
-      // console.log("filesArray: ", filesArray);
+  const style = useMemo(
+    () =>
+      ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {}),
+      }[(isDragActive, isDragReject)])
+  );
 
-      setSelectedFiles((prevImages) => prevImages.concat(filesArray));
-      Array.from(e.target.files).map(
-        (file) => URL.revokeObjectURL(file) // To avoid memory leak
-      );
-    }
-  };
+  const sourceImages = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img src={file.preview} style={img} />
+      </div>
+    </div>
+  ));
 
-  const renderPhotos = (source) => {
-    console.log("source: ", source);
-    return source.map((photo) => {
-      return <img src={photo} alt="" key={photo} />;
-    });
-  };
-
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
   return (
     <>
       <div class="split left">
@@ -35,12 +120,31 @@ const Upload = () => {
           <h1>Cheque parsing made easy, Just one click, Try now</h1>
         </div>
         <div className="input">
-          <input
-            type="file"
-            accept="image/*"
-            multiple
-            onChange={handleImageChange}
-          />
+          <div {...getRootProps({ style })}>
+            <input {...getInputProps()} />
+            <div className="drag-div">
+              <CloudUploadIcon
+                className="icon"
+                style={{
+                  fontSize: "100px",
+                }}
+              />
+              <Button
+                type="button"
+                onClick={open}
+                variant="outlined"
+                id="btn"
+                sx={{
+                  boxShadow: 5,
+                  width: 400,
+                  height: 50,
+                }}
+              >
+                Choose files to upload
+              </Button>
+              <p id="pTag">Or Drag and Drop them here</p>
+            </div>
+          </div>
         </div>
         <div className="list">
           <a href="#">
@@ -64,7 +168,7 @@ const Upload = () => {
       <div class="split right">
         <div className="cheque">
           <h1>Cheque</h1>
-          <p>{renderPhotos(selectedFiles)}</p>
+          <p style={thumbsContainer}>{sourceImages}</p>
         </div>
         <div className="output">
           <a href="#">
